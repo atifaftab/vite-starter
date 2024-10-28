d23f7e9e9be2e2b4e0e28c0f3f81251e013081ec
Atif <iamatif96@gmail.com>
2024-10-28 16:11:50 +0800

    first test for button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
-    <div><button className="red">change to blue</button></div>
+    <div><button className={buttonColor} onClick={() => setButtonColor(nextColor)}>change to {nextColor}</button></div>
   );
 }
 
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 import { expect } from "vitest";
+// import {logRoles} from '@testing-library/dom'
 
-test("button starts with correct color", () =>{
+
+test("button click flow", () =>{
+  // for debugging the test for getByRoles
+  // const { container } = render(<App />)
+  // logRoles(container);
+
+  // render the app
   render(<App/>)
-  const buttonElement = screen.getByRole("button");
+
+  //find the button
+  const buttonElement = screen.getByRole("button", {name: /blue/i});
+
+  //check initial color
   expect(buttonElement).toHaveClass("red");
-})
-test("button starts with correct text", () =>{
-  
-})
-test("button has correct color after click", () =>{
+
+  //click the button
+  fireEvent.click(buttonElement)
+
+  //check the button text
+  expect(buttonElement).toHaveTextContent(/red/i);
+  //check the button color
+  expect(buttonElement).toHaveClass("blue");
+
+
 
 })
-test("button has correct text after click", () =>{
-  
+
+test("button has correct label and color after click", () =>{
+
+
 })
